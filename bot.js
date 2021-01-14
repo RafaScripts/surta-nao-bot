13597fa404075e74a652c6f81bf292af06ba3d08
Flavio Filipe <flaviofilipe.dev@gmail.com>
2021-01-14 17:40:48 -0300

    Respondendo quando mencionado

--- bot.js
+++ bot.js
@@ -2,20 +2,36 @@ const config = require('./config');
 const twit = require('twit')
 const Twit = twit(config);
 
-function searchTweets(params){
+//Setting up a user stream
+var stream = Twit.stream('statuses/filter', { track: '@surtanao_bb' });
 
-    Twit.get('search/tweets', params, (err, data, response) => {
-        let tweets = data.statuses;
-        if(!err) {
-            for(let dat of tweets){
-                let retweetId = dat.id_str;
-                console.log(retweetId);
-            }
-        }else{
-            console.log(err);
+stream.on('tweet', tweetEvent);
+
+function tweetEvent(tweet) {
+    // console.log(tweet)
+    var reply_to = tweet.in_reply_to_screen_name;
+    var text = tweet.text;
+    var from = tweet.user.screen_name;
+    var nameID = tweet.id_str;
+    // params just to see what is going on with the tweets
+    var params = { reply_to, text, from, nameID };
+    console.log(params);
+
+    let new_tweet = "It's me!!";
+    if (from !== 'surtanao_bb') {
+        new_tweet = 'Surta não @'+from+'!!! Ouça agora a nova música de Wander Luiz: https://bit.ly/SurtaNao';
+    }
+
+    var tweet = {
+        status: new_tweet,
+        in_reply_to_status_id: nameID
+    }
+
+    Twit.post('statuses/update', tweet, (err) => {
+        if (err) {
+            console.log("Something went wrong!", err);
+        } else {
+            console.log("It worked!");
         }
-    })
+    });
 }
-
-console.log('Aqui!')
-searchTweets({q: '#javascript'})
