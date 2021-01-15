caff222b10219396b668437986b582237c90bb26
Flavio Filipe <flaviofilipe.dev@gmail.com>
2021-01-15 16:42:03 -0300

    Add random messages

--- bot.js
+++ bot.js
@@ -4,12 +4,23 @@ const Twit = twit(config);
 
 //Setting up a user stream
 var stream = Twit.stream('statuses/filter', { track: '@surtanao_bb' });
-var stream_tag = Twit.stream('statuses/filter', { track: ['#surtanao', '#surta_nao', '#SurtaNao', '#SurtaNão'] });
+var stream_tag = Twit.stream('statuses/filter', { track: ['#surtanao', '#surta_nao', '#SurtaNao', '#SurtaNão', '#SurtaNaoTeste'] });
+
+var messages = [
+    'Se liga na música',
+    'Vem com Wander Luiz!!',
+    'Ouve aí',
+    'Já foi lá curtir??',
+    'Essa é Hit!!!',
+    'Corre lá:', 
+    'Vem q é sucesso'
+];
 
 stream.on('tweet', tweetEvent);
 stream_tag.on('tweet', (tweet) => tweetEvent(tweet));
 
 function tweetEvent(tweet) {
+    const message = messages[Math.floor(Math.random() * messages.length)];
     // console.log(tweet)
     var reply_to = tweet.in_reply_to_screen_name;
     var text = tweet.text;
@@ -21,7 +32,7 @@ function tweetEvent(tweet) {
 
     let new_tweet = "It's me!!";
     if (from !== 'surtanao_bb') {
-        new_tweet = 'Surta não @'+from+'!!! Se liga na música https://sl.onerpm.com/7032476516?_ga=2.229323420.1417882663.1610680040-814832288.1609726552';
+        new_tweet = 'Surta não @'+from+' '+message+ ' https://sl.onerpm.com/7032476516?_ga=2.229323420.1417882663.1610680040-814832288.1609726552';
     }
 
     var tweet = {
